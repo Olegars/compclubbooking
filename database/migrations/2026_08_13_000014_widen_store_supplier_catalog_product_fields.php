06717d235aff809a134500fbb829698490793abe
Oleg <oleg9712949@gmail.com>
2026-09-15 11:19:31 +0300

    chat_avito7

--- database/migrations/2026_08_13_000014_widen_store_supplier_catalog_product_fields.php
+++ database/migrations/2026_08_13_000014_widen_store_supplier_catalog_product_fields.php
@@ -20,12 +20,13 @@ return new class extends Migration
             DB::statement('ALTER TABLE store_supplier_catalog_products ALTER COLUMN part TYPE TEXT');
             DB::statement('ALTER TABLE store_supplier_catalog_products ALTER COLUMN vendor TYPE VARCHAR(512)');
             DB::statement('ALTER TABLE store_supplier_catalog_products ALTER COLUMN warranty TYPE VARCHAR(64)');
-        } else {
+        } elseif ($driver === 'mysql' || $driver === 'mariadb') {
             DB::statement('ALTER TABLE store_supplier_catalog_products MODIFY name TEXT NOT NULL');
             DB::statement('ALTER TABLE store_supplier_catalog_products MODIFY part TEXT NULL');
             DB::statement('ALTER TABLE store_supplier_catalog_products MODIFY vendor VARCHAR(512) NULL');
             DB::statement('ALTER TABLE store_supplier_catalog_products MODIFY warranty VARCHAR(64) NULL');
         }
+        // sqlite: affinity, длины VARCHAR не режет
     }
 
     public function down(): void
